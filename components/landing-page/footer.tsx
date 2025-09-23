471f4e6c7325a985ee4536a63aec315eb43634de
abraham <code.arb2712@gmail.com>
2025-09-23 15:19:22 -0400

    add contact data

--- components/landing-page/footer.tsx
+++ components/landing-page/footer.tsx
@@ -21,16 +21,13 @@ export default function Footer() {
   return (
     <footer className="container py-8 border-t border-gray-200 dark:border-gray-800">
       <div className="flex flex-col items-center text-center">
-        <Link
-          href="/"
-          className="flex flex-col text-gray-400 items-center justify-center mb-4 text-[12px]"
-        >
+        <div className="flex flex-col text-gray-400 items-center justify-center mb-4 text-[12px]">
           <h3 className="font-semibold text-2xl text-[#111928]">
             CONEXIONES INNOVER
           </h3>
           <p className="mt-1">Rif: J 50316437-9</p>
           <p className="mt-1">Innover.ca@gmail.com</p>
-        </Link>
+        </div>
         <p className="text-sm text-gray-600 dark:text-gray-400 max-w-md mx-auto mb-8">
           Tu socio tecnológico en internet, innovación y conectividad sin
           límites.
